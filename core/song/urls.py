93d993dab94aed92b493b11d0a7e63de4c8d3910
reza.latifi <reza72rg@gmail.com>
2024-01-08 20:45:46 +0330

    V "1.5"
    In this commit I changed templates

--- core/song/urls.py
+++ core/song/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
-from song.views import HomeList, PlaySong
+from song.views import HomeList, PlaySong, ChoiceArtist, AllSong
 
 app_name = "song"
 
 urlpatterns = [
     # Task list view
     path("", HomeList.as_view(), name="home"),
+    path('all_song/', AllSong.as_view(), name='all_song'),
+    path('artist-choice/<int:artist_id>/', ChoiceArtist.as_view(), name='artist_choice'),
     path('play_song/<int:song_id>/', PlaySong.as_view(), name='play_song'),
 ]
--- core/song/views.py
+++ core/song/views.py
@@ -18,11 +18,22 @@ class HomeList(ListView):
         context["all_songs"] = Song.objects.all()
         return context
     
-
+class AllSong(ListView):
+    model = Song
+    context_object_name = "all_song"
+    template_name = "song/all-song.html"
     
 class PlaySong(View):
     def get(self, request, song_id):
         song = get_object_or_404(Song, pk= song_id)
         context = {'song':song}
-        return render (request , 'song/song.html',context)
+        return render (request , 'song/detail.html',context)
         
+class ChoiceArtist(View):
+    def get(self, request, artist_id):
+        artist = get_object_or_404(Artist, pk= artist_id)
+      
+        songs = Song.objects.filter(artist=artist.id)
+       
+        context = {'all_songs':songs}
+        return render (request , 'song/songs.html',context)
