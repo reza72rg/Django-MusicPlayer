cfe97a4879747848744050c17dce52c1c5c2c008
reza.latifi <reza72rg@gmail.com>
2024-01-13 21:34:06 +0330

    V "1.12"
    In this commit I add test for models

--- core/song/tests/tests_song_models.py
+++ core/song/tests/tests_song_models.py
@@ -2,19 +2,20 @@ from django.test import TestCase
 from django.urls import reverse, resolve
 from django.contrib.auth.models import User
 from song.models import Song, Artist, Category
-from yourapp.views import HomeList  # Import the HomeList view from your app
+
 
 class TestSongModels(TestCase):
+    def setUp(self):
+        self.artist = Artist.objects.create(name='test')
+        self.category_obj = Category.objects.create(name="test")
+        self.author = User.objects.create(username="test", password="123456789ab")
     def test_create_song_with_valid_data(self):
-        artist = Artist.objects.create(name='test')
-        category = Category.objects.create(name="test")
-        author = User.objects.create(username="test", password="123456789ab")
         song = Song.objects.create(
             title="test",
-            author=author,
-            artist=artist,
-            category=category,
-        )
-        self.assertEqual(resolve(url).func.view_class,HomeList)
+            author=self.author,
+            artist=self.artist,)
+        song.category.set([self.category_obj])
+        self.assertTrue(Song.objects.filter(pk=song.id).exists())
+        self.assertEqual(song.author.username,"test")
 
     
